36a8408f33bd0a9564f762d100ac0a872865f5e1
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-17 18:24:25 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    if ((pickup - 42)>=0) {
+        return pickup -42
+    } else {
+        return 42 - pickup
+    }
+}
+
+function distanceFromHqInFeet (pickup) {
+    return distanceFromHqInBlocks(pickup) *  264
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start >= end) {
+        return (start - end) * 264
+    } else return (end - start) * 264
+}
+
+function calculatesFarePrice(start, end) {
+    let feet = distanceTravelledInFeet(start, end)
+    if (feet <= 400) {
+        return 0
+    } else if (feet <2000) {
+        return Number.parseFloat((feet-400)*0.02)
+    } else if (feet <2500) {
+        return 25
+    } else if (feet > 2500) {
+        return "cannot travel that far"
+    }
+}
